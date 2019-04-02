30038f8fe17531ca36f3824b0d877afbfe01c069
tksgi <tksgi12@gmail.com>
2019-04-02 15:57:57 +0900

    エラー時に404ページを表示する

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  #例外処理
+  rescue_from ActiveRecord::RecordNotFound, with: :render404
+  rescue_from Exception, with: :render404
+
+  def render404
+    render template: 'public/404.html'
+  end
 end
