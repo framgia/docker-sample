c2bb0c98b190fe34beb0223bac5e6e1b4d20e208
tksgi <tksgi12@gmail.com>
2019-04-02 19:02:59 +0900

    削除機能view実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -68,6 +68,7 @@ class UsersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
+      binding.pry
       @user = User.available.find(params[:id])
     end
 
