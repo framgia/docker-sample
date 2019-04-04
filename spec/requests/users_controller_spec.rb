7a4f68495ad33c3620268eca7fdaf6d4f9d25eb3
Norifumi Homma <norifumi.homma@gmail.com>
2019-04-04 11:50:49 +0900

    show の spec 追加

--- spec/requests/users_controller_spec.rb
+++ spec/requests/users_controller_spec.rb
@@ -3,6 +3,7 @@ require "rails_helper"
 RSpec.describe UsersController, type: :request do
   let!(:available_user){create :user}
   let!(:unavailable_user){create :user, :unavailable}
+  
   describe "GET #index" do
     it do
       get users_url
@@ -12,11 +13,23 @@ RSpec.describe UsersController, type: :request do
   describe "GET #show" do
     context "id exists" do
       context "user is available" do
+        it do
+          get user_url(id: available_user.id)
+          expect(response.status).to eq 200
+        end
       end
       context "user isn't available" do
+        it do
+          get user_url(id: unavailable_user.id)
+          expect(response.status).to eq 404
+        end
       end
     end
     context "id doesn't exist" do
+      it do
+        get user_url(id: "x123")
+        expect(response.status).to eq 404
+      end
     end
   end
 end
