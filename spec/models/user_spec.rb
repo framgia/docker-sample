f79cf6b3a54f0499300a66d791146e1f2f1b089e
tksgi <tksgi12@gmail.com>
2019-04-04 10:56:57 +0900

    modelのspec作成中

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,4 +13,17 @@ describe User, type: :model do
       it{is_expected.to be_truthy}
     end
   end
+
+  describe '#unavailable' do
+    let(:user){create :user, archived_at: archived_at}
+    let(:archived_at){nil}
+    subject{user.unavailable}
+    context "archived_at is set" do
+      let(:archived_at){Time.zone.now}
+      it{is_expected.not_to }
+    end
+    context "archived_at isn't set" do
+      it{is_expected.not_to be_nil}
+    end
+  end
 end
