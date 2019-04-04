18475ad03941b98932e79bb2616f325851dc2b63
Norifumi Homma <norifumi.homma@gmail.com>
2019-04-04 14:28:53 +0900

    [WIP] refs #16 論理削除機能の spec を書く (#17)
    * refs #16 論理削除機能の spec を書く
    
    * refs #16 論理削除機能の spec を書く
    
    * modelのspec作成中
    
    * unavailableのspecを作成
    
    * user#showのspec作成開始
    
    * show の spec 追加
    
    * user#destroyのspec作成中 context
    
    * users#destroyのspec作成中
    
    * ユーザー作成処理を追加
    
    * user#destroy rspec追加
    
    * reload
    
    * user#destroy spec作成完了

---
+++ spec/requests/users_controller_spec.rb
@@ -0,0 +1,98 @@
+require "rails_helper"
+
+RSpec.describe UsersController, type: :request do
+  let!(:available_user){create :user}
+  let!(:unavailable_user){create :user, :unavailable}
+  
+  describe "GET #index" do
+    it do
+      get users_url
+      expect(response.status).to eq 200
+    end
+  end
+  describe "GET #show" do
+    context "id exists" do
+      context "user is available" do
+        it do
+          get user_url(id: available_user.id)
+          expect(response.status).to eq 200
+        end
+      end
+      context "user isn't available" do
+        it do
+          get user_url(id: unavailable_user.id)
+          expect(response.status).to eq 404
+        end
+      end
+    end
+    context "id doesn't exist" do
+      it do
+        get user_url(id: "x123")
+        expect(response.status).to eq 404
+      end
+    end
+  end
+
+  describe "DELETE #destroy" do
+    let!(:available_user1){create :user}
+    let!(:available_user2){create :user}
+    let!(:unavailable_user1){create :user, :unavailable}
+
+    context "id exists" do
+      context "user is available" do
+        context "there is one user" do
+          it do
+            delete user_url(id: available_user1.id)
+            expect(response).to redirect_to(users_url)
+          end
+          it do
+            delete user_url(id: available_user1.id)
+            expect(available_user1.reload.archived_at).not_to be_nil
+          end
+        end
+        context "there are multiple user" do
+          context "there are only available user" do
+            it do
+              delete user_url(id: "#{available_user1.id},#{available_user2.id}")
+              expect(response).to redirect_to(users_url)
+            end
+            it do
+              delete user_url(id: "#{available_user1.id},#{available_user2.id}")
+              expect(available_user1.reload.available?).to be_falsey
+            end
+            it do
+              delete user_url(id: "#{available_user1.id},#{available_user2.id}")
+              expect(available_user2.reload.available?).to be_falsey
+            end
+          end
+          context "there ain't only available user" do
+            it do
+              delete user_url(id: "#{available_user1.id},#{unavailable_user1},#{available_user2.id}")
+              expect(response.status).to eq 404
+            end
+            it do
+              delete user_url(id: "#{available_user1.id},#{unavailable_user1},#{available_user2.id}")
+              expect(available_user1.reload.available?).to be_truthy
+            end
+            it do
+              delete user_url(id: "#{available_user1.id},#{unavailable_user1},#{available_user2.id}")
+              expect(available_user2.reload.available?).to be_truthy
+            end
+          end
+        end
+      end
+      context "user isn't available" do
+        it do
+          delete user_url(id: unavailable_user.id)
+          expect(response.status).to eq 404
+        end
+      end
+    end
+    context "id doesn't exist" do
+      it do
+        delete user_url(id: "x123")
+        expect(response.status).to eq 404
+      end
+    end
+  end
+end
